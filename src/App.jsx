9ff7831ad47784128b048972cfb520a325615929
Aminul0001 <i.aminul0001@gmail.com>
2024-03-25 10:48:53 +0600

    One way to submit form

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+import SimpleForm from './Componets/SimpleForm/SimpleForm'
+
+function App() {
+
+
+  return (
+    <>
+  
+      <h1>Form Master</h1>
+      <SimpleForm></SimpleForm>
+
+    </>
+  )
+}
+
+export default App
---
+++ src/Componets/SimpleForm/SimpleForm.jsx
@@ -0,0 +1,22 @@
+
+
+const SimpleForm = () => {
+    const handleSubmit = e => {
+        e.preventDefault()
+        console.log(e.target.name.value)
+        console.log(e.target.email.value)
+    }
+    return (
+        <div>
+            
+        <form action="" onSubmit={handleSubmit}>
+            <input type="text" name="name" /> <br />
+            <input type="email" name="email" /> <br />
+            <input type="submit" name="submit" />
+        </form>
+
+        </div>
+    );
+};
+
+export default SimpleForm;
